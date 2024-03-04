33c5bb7bfba935090480409bc69fe974ad5774f2
vtlnr <149604564+vtlnr@users.noreply.github.com>
2024-03-04 18:22:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,45 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    if (value <= 1) {
+        return false;
+    }
+    for (uint64_t i = 2; i * i <= value; i++) {
+        if (value % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
-
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  if (n == 1) {
+    return 2;
+  }
+  uint64_t number = 3;
+  uint64_t count = 1;
+  while (count < n) {
+    if (checkPrime(number)) {
+      count++;
+    }
+    if (count < n) {
+      number += 2;
+    }
+  }
+  return number;
 }
-
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  uint64_t null = value + 1;
+  while (!checkPrime(null)) {
+    null++;
+  }
+  return null;
 }
-
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t summa = 0;
+  for (uint64_t i =2; i < hbound; i++) {
+    if (checkPrime(i)) {
+      summa += i;
+    }
+  }
+  return summa;
 }
